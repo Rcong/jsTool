ef9676fbccb90a8026ece2cd7ffb04f7edfaf474
Rcong <654846387@qq.com>
2016-07-15 23:18:24 +0800

    修改deepCopy

--- jsTool.js
+++ jsTool.js
@@ -85,40 +85,50 @@ var jsTool = (function(){
             }
             return newArray;
         },
-        /* 克隆 */
-        cloneObj: function(src) {
-            var clone = src;
+        // 判断是否是空对象
+        isEmptyObject: function(obj) {
+            var name;
+            for ( name in obj ) {
+                return false;
+            }
+            return true;
+        },
+        //深拷贝
+        deepCopy: function(obj) {
+            var copy = obj;
 
                // 对于Date,String,Boolean等引用类型的数据，需要考虑调用构造函数重新构造，直接赋值依然会有引用问题（不是真正的clone引用变量）
+
                // 对于 Date
-               if (src instanceof Date) {
-                   clone = new Date(src.getDate());
-                   return clone;
+               if (obj instanceof Date) {
+                   copy = new Date(obj.getDate());
+                   return copy;
                }
 
                // 对于Object和Array的遍历，可以使用for in，这样可以保证在在Array对象上扩展的属性也可以正确复制
                // 对于 数组
-               if (src instanceof Array) {
-                   clone = [];
-                   for (var key in src) {
-                       clone[key] = cloneObj(src[key]);
+               if (obj instanceof Array) {
+                   copy = [];
+                   for (var key in obj) {
+                       copy[key] = deepCopy(obj[key]);
                    }
-                   return clone;
+                   return copy;
                }
 
                // 对于 Object
-               if (src instanceof Object) {
-                   clone = {};
-                   for (var key in src) {
-                       if (src.hasOwnProperty(key)) {       // 忽略掉继承属性
-                           clone[key] = cloneObj(src[key]);
+               if (obj instanceof Object) {
+                   copy = {};
+                   for (var key in obj) {
+                       //判断属性是否是原型链上的,本身属性才copy
+                       if (obj.hasOwnProperty(key)) {
+                           copy[key] = deepCopy(obj[key]);
                        }
                    }
-                   return clone;
+                   return copy;
                }
 
                // 对于 数字 字符串 布尔 null undefined
-               return src;
+               return obj;
         },
         /* DOM操作 */
         //去除字符串的空白字符
